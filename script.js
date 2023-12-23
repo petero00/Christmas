376d62dbd8e4a1411bf7eb82fb4ccf3bfccaf937
petero00 <145260611+petero00@users.noreply.github.com>
2023-12-23 21:35:17 +0900

    Create script.js

---
+++ script.js
@@ -0,0 +1,20 @@
+let nowTime = new Date();
+let hiniti = nowTime.getDate();
+
+keizoku();
+setInterval('keizoku()',1000);
+
+function santa() {
+    document.getElementById('santa').style.animationPlayState = 'running';
+    document.getElementById('HO').style.display = 'block';
+}
+
+
+
+
+function keizoku() {
+    if (hiniti === 25) {
+        document.getElementById('tree').src = 'https://4.bp.blogspot.com/-xC3fKIfunTo/VkqPNryifwI/AAAAAAAA0ho/kWbucZsAKwU/s800/christmastree_decoration.png';
+        document.getElementById('snow').style.display = 'block';
+    }
+}
